096a7325358f35eb3b7a504fdd841f544d41ab83
Scott Herrmann <scott.herrmann@sheakley.com>
2019-03-21 10:53:02 -0400

    Initial build

---
+++ LinuxHostingService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+
+namespace SampleLinuxService {
+    public class LinuxHostingService : IHostedService {
+        IApplicationLifetime appLifetime;
+        //ILogger<LinuxHostingService> logger;
+        IHostingEnvironment environment;
+        IConfiguration configuration;
+        
+
+        public LinuxHostingService (IConfiguration configuration,
+                                    IHostingEnvironment environment,
+                                    ILogger<LinuxHostingService> logger,
+                                    IApplicationLifetime appLifetime
+                                    ) {
+            this.configuration = configuration;
+            
+            
+            //this.logger = logger;
+            
+
+            //Log.Logger = new LoggerConfiguration();
+            LoggerConfiguration lc = new LoggerConfiguration();
+            lc.WriteTo.Console();
+            lc.WriteTo.File ("C:\\temp\\log.txt");
+            //lc.CreateLogger();
+            Log.Logger = lc.CreateLogger();
+
+            
+            //Log.Logger = new LoggerConfiguration().CreateLogger();
+            
+            
+
+            this.appLifetime = appLifetime;
+            this.environment = environment;
+
+
+            // A:  Retrieve an environment variable
+            string sampleDB = configuration.GetValue<string> ("DB");
+            Console.WriteLine("Environment variable [SAMPLE_DB] = " + sampleDB);
+
+
+            // B:  Retrieve a config variable from the appsettings.json file - the non environment specific one.
+            string globalSetting1 = configuration.GetValue<string> ("FromAppSettingBase:setting1");
+            Console.WriteLine("Value from AppSettings.json:  [FromAppSettingBase:setting1] = " + globalSetting1);
+
+
+            // C:  Retrieve a setting originall defined in appsettings.json file, but also exists in appsettings.[environment].json file.
+            //     Will contain value from the environment version of the appsettings file.
+            //     Value should be "Second"
+            string overriddenSetting = configuration.GetValue<string> ("WillBeOverridden");
+            Console.WriteLine("Overridden Value from appsettings.development.json:  [WillBeOverridden] = " + overriddenSetting);
+
+
+            // D:  Retrieve a setting only defined in the environment specific version of the file - appsettings.development.json file.
+            string onlyInDev = configuration.GetValue<string> ("UniqueToDevelopment");
+            Console.WriteLine("Value only defined in appsettings.development.json file:  [UniqueToDevelopment] = " + onlyInDev);
+
+            // Retrieve an Appsettings variable
+
+            string connection = configuration.GetValue<string> ("SampleDB:Connection");
+            
+        }
+
+
+        public Task StartAsync (CancellationToken cancellationToken) {
+            Log.Information("StartAsync method called.");
+//            this.logger.LogInformation ("StartAsync method called.");
+
+            this.appLifetime.ApplicationStarted.Register (OnStarted);
+            this.appLifetime.ApplicationStopping.Register (OnStopping);
+            this.appLifetime.ApplicationStopped.Register (OnStopped);
+
+            return Task.CompletedTask;
+
+        }
+
+
+        private void OnStarted () {
+            Log.Information("OnStarted called.");
+            //this.logger.LogInformation ("OnStarted method called.");
+
+            // Post-startup code goes here  
+        }
+
+
+        private void OnStopping () {
+            Log.Information ("OnStopped method called");
+            //this.logger.LogInformation ("OnStopping method called.");
+
+            // On-stopping code goes here  
+        }
+
+        private void OnStopped() {
+            Log.Information ("OnStopped method called");
+            Log.CloseAndFlush();
+                //this.logger.LogInformation("OnStopped method called.");
+
+            // Post-stopped code goes here  
+        }
+
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Log.Information("StopAsync method called");
+            //this.logger.LogInformation("StopAsync method called.");
+
+            
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+
+namespace SampleLinuxService
+{
+    /// <summary>
+    /// This program is a sample app that shows how to use the HostBuilder to setup and run a service oriented application. Below the square brackets
+    /// indicate where in the program this is demonstrated (Mostly in the LinuxHostingService class).
+    /// It demonstrates the following things:
+    ///  - You must have the environment variable  ASPNETCORE_ENVIRONMENT = development set in order for it to read the appsettings.development.json file.
+    ///    * In production this would be set to Production to read an appsettings.production.json file.
+    ///  - It will read environment variables
+    ///    * It will load any environment variable that is prefixed with SAMPLE_.  The prefix is removed and just a variable
+    ///      with the part after the prefix is created.  For example if you in your environment set a variable SAMPLE_DB = aTest
+    ///      then you will have a variable name DB set to "aTest".
+    ///    * We read the environment variable in the LinuxHostingService class.
+    ///  - [B] It will read settings from an AppSettings.json file using a JSON file Reader (Example B: in LinuxHostingService)
+    ///  - [C, D] It will read settings from an AppSettings.Development.json file using a JSON file reader [D].  Any settings with the same
+    ///    name that existed in the non environment specific AppSettings.Json file will be overriden with the value from the environment
+    ///    specific version.  See Example C:
+    ///  
+    /// </summary>
+    class Program
+    {
+/*        public static Action<IConfigurationBuilder> BuildConfig = builder => builder
+                                                                             .SetBasePath(Directory.GetCurrentDirectory())
+                                                                             .AddJsonFile("hostsettings.json", optional: true)
+                                                                             .AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                                                                             */
+        static async Task Main(string[] args)
+        {
+/*
+            var builder = new ConfigurationBuilder();
+            BuildConfig (builder);
+
+            builder.AddCommandLine (args);
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration (builder.Build()).WriteTo.Console().CreateLogger();
+
+            builder
+            */
+
+
+
+
+            IHost host = new HostBuilder()
+                            // TODO Not sure we need this first ConfigureHostConfiguration as its interface is deprecated and it just passes its values onto the ConfigureAppConfiguration parameters anyway.
+                                     .ConfigureHostConfiguration(configHost =>
+                                     {
+                                         configHost.SetBasePath(Directory.GetCurrentDirectory());
+                                         configHost.AddJsonFile("hostsettings.json", optional: true);
+                                         configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+                                         configHost.AddCommandLine(args);
+                                     })
+
+                                     .ConfigureAppConfiguration((hostContext, configApp) =>
+                                     {
+                                         configApp.SetBasePath(Directory.GetCurrentDirectory());
+                                         configApp.AddJsonFile($"appsettings.json", true);
+                                         configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+                                         configApp.AddEnvironmentVariables(prefix: "SAMPLE_");
+                                         configApp.AddCommandLine(args);
+                                     })
+                                     .ConfigureServices((hostContext, services) =>
+                                     {
+                                         services.AddLogging();
+                                         services.AddHostedService<LinuxHostingService>();
+                                     })
+                                     
+                                     .ConfigureLogging((hostContext, configLogging) =>
+                                     {
+    
+    
+                                         configLogging.AddSerilog(new LoggerConfiguration()
+    
+                                                                  .ReadFrom.Configuration(hostContext.Configuration)
+                                                                  .CreateLogger());
+    
+                                         configLogging.AddConsole();
+                                         configLogging.AddDebug();
+                                     })
+                                     
+
+                                     .Build();
+    
+                        await host.RunAsync();
+                        Console.ReadKey();
+        }
+    }
+}
