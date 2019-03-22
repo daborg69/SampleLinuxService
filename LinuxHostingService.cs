9aadacdf164e4f531151f10c3b6c7f1cc85a5ea7
Scott Herrmann <scott.herrmann@sheakley.com>
2019-03-22 05:03:26 -0400

    Error handling mostly done.

--- LinuxHostingService.cs
+++ LinuxHostingService.cs
@@ -9,6 +9,10 @@ using Serilog;
 
 
 namespace SampleLinuxService {
+	/// <summary>
+	/// This is the actual application service that is going to be running.  It should have complete error detection logic at every method and routine to prevent
+	/// unlogged and hidden errors.
+	/// </summary>
     public class LinuxHostingService : IHostedService {
         IHostApplicationLifetime appLifetime;
         ILogger<LinuxHostingService> _logger;
@@ -21,37 +25,42 @@ namespace SampleLinuxService {
                                     ILogger<LinuxHostingService> logger,
                                     IHostApplicationLifetime appLifetime
                                     ) {
-            this.configuration = configuration;
-            this._logger = logger;
-            this.appLifetime = appLifetime;
-            this.environment = environment;
+	        try {
+		        this.configuration = configuration;
+		        this._logger = logger;
+		        this.appLifetime = appLifetime;
+		        this.environment = environment;
 
 
-            // A:  Retrieve an environment variable
-            string sampleDB = configuration.GetValue<string> ("DB");
-            Console.WriteLine("Environment variable [SAMPLE_DB] = " + sampleDB);
+		        // A:  Retrieve an environment variable
+		        string sampleDB = configuration.GetValue<string>("DB");
+		        Console.WriteLine("Environment variable [SAMPLE_DB] = " + sampleDB);
 
 
-            // B:  Retrieve a config variable from the appsettings.json file - the non environment specific one.
-            string globalSetting1 = configuration.GetValue<string> ("FromAppSettingBase:setting1");
-            Console.WriteLine("Value from AppSettings.json:  [FromAppSettingBase:setting1] = " + globalSetting1);
+		        // B:  Retrieve a config variable from the appsettings.json file - the non environment specific one.
+		        string globalSetting1 = configuration.GetValue<string>("FromAppSettingBase:setting1");
+		        Console.WriteLine("Value from AppSettings.json:  [FromAppSettingBase:setting1] = " + globalSetting1);
 
 
-            // C:  Retrieve a setting originally defined in appsettings.json file, but also exists in appsettings.[environment].json file.
-            //     Will contain value from the environment version of the appsettings file.
-            //     Value should be "Second"
-            string overriddenSetting = configuration.GetValue<string> ("WillBeOverridden");
-            Console.WriteLine("Overridden Value from appsettings.development.json:  [WillBeOverridden] = " + overriddenSetting);
+		        // C:  Retrieve a setting originally defined in appsettings.json file, but also exists in appsettings.[environment].json file.
+		        //     Will contain value from the environment version of the appsettings file.
+		        //     Value should be "Second"
+		        string overriddenSetting = configuration.GetValue<string>("WillBeOverridden");
+		        Console.WriteLine("Overridden Value from appsettings.development.json:  [WillBeOverridden] = " + overriddenSetting);
 
 
-            // D:  Retrieve a setting only defined in the environment specific version of the file - appsettings.development.json file.
-            string onlyInDev = configuration.GetValue<string> ("UniqueToDevelopment");
-            Console.WriteLine("Value only defined in appsettings.development.json file:  [UniqueToDevelopment] = " + onlyInDev);
+		        // D:  Retrieve a setting only defined in the environment specific version of the file - appsettings.development.json file.
+		        string onlyInDev = configuration.GetValue<string>("UniqueToDevelopment");
+		        Console.WriteLine("Value only defined in appsettings.development.json file:  [UniqueToDevelopment] = " + onlyInDev);
 
-            // Retrieve an Appsettings variable
+		        // Retrieve an Appsettings variable
 
-            string connection = configuration.GetValue<string> ("SampleDB:Connection");
-            
+		        string connection = configuration.GetValue<string>("SampleDB:Connection");
+	        }
+	        catch ( Exception e ) {
+		        string message = "Unexpected error occurred in the application: " + environment.ApplicationName;
+				_logger.LogCritical(e,"This was an unhandled error.");
+	        }
         }
 
 
@@ -83,8 +92,20 @@ namespace SampleLinuxService {
         private void OnStarted () {
             _logger.LogInformation("OnStarted Method started.");
 
-            // Post-startup code goes here  
+
+			// Post-startup code goes here  
+
+			// The following should be deleted in normal apps.  For demonstration purposes only.
+			Console.WriteLine("Press the E key to throw an error.  Any other key to continue normal processing.");
+			ConsoleKeyInfo key = new ConsoleKeyInfo();
+			key = Console.ReadKey();
+	        if ( key.Key == ConsoleKey.E ) {
+		        try { throw new ArgumentException("There was a problem with Argument2 - X: 15"); }
+				catch (Exception e) { UnHandledError(e);}
+	        }
         }
+
+		
 
 
         private void OnStopping () {
@@ -110,5 +131,10 @@ namespace SampleLinuxService {
 
             return Task.CompletedTask;
         }
+
+
+		private void UnHandledError (Exception e) {
+			_logger.LogCritical(e,"This error was unexpected and unhandled by the application.");
+		}
     }
 }
--- Program.cs
+++ Program.cs
@@ -36,52 +36,63 @@ namespace SampleLinuxService
     class Program
     {
 
-        static async Task Main(string[] args)
-        {
-            IHost host = new HostBuilder()
-                            // TODO Not sure we need this first ConfigureHostConfiguration as its interface is deprecated and it just passes its values onto the ConfigureAppConfiguration parameters anyway.
-                                     .ConfigureHostConfiguration(configHost =>
-                                     {
-                                         configHost.SetBasePath(Directory.GetCurrentDirectory());
-                                         configHost.AddJsonFile("hostsettings.json", optional: true);
-                                         configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
-                                         configHost.AddCommandLine(args);
-                                     })
+	    static async Task Main (string [] args) {
+		    try {
+			    IHost host = new HostBuilder()
 
-                                     .ConfigureAppConfiguration((hostContext, configApp) =>
-                                     {
-                                         configApp.SetBasePath(Directory.GetCurrentDirectory());
-                                         configApp.AddJsonFile($"appsettings.json", true);
-                                         configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
-                                         configApp.AddEnvironmentVariables(prefix: "SAMPLE_");
-                                         configApp.AddCommandLine(args);
-                                     })
-                                    .ConfigureLogging((hostContext, configLogging) =>
-                                    {
-                                        configLogging.AddSerilog(new LoggerConfiguration()
-                                                                 .ReadFrom.Configuration(hostContext.Configuration)
-                                                                 .CreateLogger());
+			                 // TODO Not sure we need this first ConfigureHostConfiguration as its interface is deprecated and it just passes its values onto the ConfigureAppConfiguration parameters anyway.
+			                 .ConfigureHostConfiguration(configHost => {
+				                 configHost.SetBasePath(Directory.GetCurrentDirectory());
+				                 configHost.AddJsonFile("hostsettings.json", optional: true);
+				                 configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+				                 configHost.AddCommandLine(args);
+			                 })
+			                 .ConfigureAppConfiguration((hostContext, configApp) => {
+				                 configApp.SetBasePath(Directory.GetCurrentDirectory());
+				                 configApp.AddJsonFile($"appsettings.json", true);
+				                 configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+				                 configApp.AddEnvironmentVariables(prefix: "SAMPLE_");
+				                 configApp.AddCommandLine(args);
+			                 })
+			                 .ConfigureLogging((hostContext, configLogging) => {
+				                 configLogging.AddSerilog(new LoggerConfiguration().ReadFrom.Configuration(hostContext.Configuration).CreateLogger());
+
 // We NEVER want to log to Console in production - well at least if it is an API or Web app we do not - it will kill performance.
 #if DEBUG
-                                        configLogging.AddConsole();
-                                        configLogging.AddDebug();
-#endif                                        
-                                    })
+				                 configLogging.AddConsole();
+				                 configLogging.AddDebug();
+#endif
+			                 })
+			                 .ConfigureServices((hostContext, services) => {
+				                 services.AddLogging();
+				                 services.AddHostedService<LinuxHostingService>();
+			                 })
+			                 .Build();
 
+			    await host.RunAsync();
 
-                                     .ConfigureServices((hostContext, services) =>
-                                     {
-                                         services.AddLogging();
-                                         services.AddHostedService<LinuxHostingService>();
-                                     })
-                                     
+				// Remove this next statement for normal programs - just used to demonstrate an error situation.
+				throw new ApplicationException("Test throwing an exception");
 
-                                     .Build();
-    
-                        await host.RunAsync();
-                        Console.WriteLine("Flushing logs");
-                        Log.CloseAndFlush();
-            Console.ReadKey();
-         }
-    }
+				// Exit the app.
+			    Console.WriteLine("Flushing logs");
+			    Log.CloseAndFlush();
+			    Console.ReadKey();
+		    } // End Try
+		    catch ( Exception e ) {
+			    string message =
+				    "An unexpected error occurred during processing.  Because of where this error occured, there most likely will be no log of this error and the error message below will be the only copy available to provide to others.";
+				Console.WriteLine(message);
+				Log.Logger.Fatal(e,message);
+				Log.CloseAndFlush();
+				Console.WriteLine("Last Error was: " + e.Message);
+				Console.WriteLine("Error specific data was: " + e.Data.ToString());
+				Console.WriteLine("Full Error: " + e.ToString());
+			    Console.WriteLine("Press the R key to exit once you have copied all error information ");
+
+			    ConsoleKeyInfo key = new ConsoleKeyInfo('A',ConsoleKey.A,false,false,false);
+			    while ( key.Key != ConsoleKey.R ) { key = Console.ReadKey(); }		    
+		    }
+	    }  // End Main Method
+    }  // End Class
 }
